92f2cd4dc29b809484eb5c9996afd198578f51bc
ClainAtomi <clainatomi.ctb@gmail.com>
2015-05-26 09:27:22 +0300

    restore password by clainatomi

--- OwnerPanel/Menu/Menu.js
+++ OwnerPanel/Menu/Menu.js
@@ -7,8 +7,8 @@ angular.module('App.OwnerPanelMenu', [
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   when('/owner/:placeId/menu/', {
-	templateUrl: 'OwnerPanel/Menu/Menu.html',
-	controller: 'OwnerPanelMenuCtrl'
+  templateUrl: 'OwnerPanel/Menu/Menu.html',
+  controller: 'OwnerPanelMenuCtrl'
   })
 }])
 
@@ -17,139 +17,140 @@ angular.module('App.OwnerPanelMenu', [
   $scope.ownerUrl = 'owner/' +  $routeParams.placeId + '/';
 
   $scope.category = {
-	 dishCategoryId: 0
+   dishCategoryId: 0
   }
   var getCatList = function() {
-	ApiRequest.get('dishcategory/list', true)
-	.success(function(data, state) {
-	  $scope.catsList = data;
-	});
+  ApiRequest.get('dishcategory/list', true)
+  .success(function(data, state) {
+    $scope.catsList = data;
+  });
   }
   getCatList();
 
   $scope.dinnersPanel = {
-	currCat: 0,
-	changeCat: function(catId) {
-	  $scope.dinnersPanel.currCat = catId;
-	  $scope.category.dishCategoryId = $scope.dinnersPanel.currCat;
-	  console.log('curr catId = ' + $scope.category.dishCategoryId);
-		console.log($scope.dinnersPanel.dinnersList[0]);
+  currCat: 0,
+  changeCat: function(catId) {
+    $scope.dinnersPanel.currCat = catId;
+    $scope.category.dishCategoryId = $scope.dinnersPanel.currCat;
+    console.log('curr catId = ' + $scope.category.dishCategoryId);
+    console.log($scope.dinnersPanel.dinnersList[0]);
 
-	},
-	state: {
-	  noData: false
-	},
-	dinnersList: {},
-	getDinnersList: function(placeId) {
-	  console.log('placeId = ' + placeId);
-	  var reqUrl = 'menu/dishlist?placeId=' + placeId;
-	  console.log('try to get comples dinners list');
-	  ApiRequest.get(reqUrl, true)
-	  .success(function(data, state) {
-		console.log('dinners list downloaded');
-		$scope.dinnersPanel.dinnersList = data;
-		if(state == 204) {
-		  console.log('no data in dinners list');
-		  $scope.dinnersPanel.state.noData = true;
-		}
-		else {
-		  $scope.dinnersPanel.state.noData = false;
-		}
-	  })
-	  .error(function(data, state) {
-		console.log('an error occured while loading dinner list');
-	  });
-	},
-	addDinner: function(dish) {
-	  ApiRequest.post('dish/new', {
-		name: dish.name,
-		description: dish.description,
-		cost: dish.cost,
-		dishCategoryId: dish.cat
-	  }, false)
-	  .success(function(data, state) {
-		SendFile({
-		  file: dish.file,
-		  path: 'dishs',
-		  type: 'dish',
-		  name: data.id + '.jpg'
-		});
-		console.log('dish has been added');
-		alert('Блюдо добавлено');
-	  })
-	  .error(function(data, state) {
-		alert('Возникла ошибка при добавлении блюда');
-		console.log('api return error while trying to add new dish');
-	  });
-	},
-	deleteDinner: function(id) {
-	  if(confirm('Вы действительно хотите удалить это блюдо?')) {
-		ApiRequest.post('dish/delete', {
-		  id: id
-		}, false)
-		.success(function(data, state) {
-		  console.log('dinner deleted');
-		  $scope.dinnersPanel.getDinnersList($routeParams.placeId);
-		})
-		.error(function(data, state) {
-		  console.log('complex dinner deleting error');
-		  alert('Возникла ошибка при удалении блюда');
-		});
-	  }
-	},
-	editDinner: function(dinner){
-	  ApiRequest.post('dish/update', {
-		id: dinner.id,
-		placeId: $routeParams.placeId,
-		name: dinner.name,
-		description: dinner.description,
-		cost: dinner.cost,
-		dishCategoryId: dinner.dishCategoryId
-	  }, false)
-	  .success(function(data, state) {
-		console.log('dinner edited');
-		if(dinner.preview != undefined && dinner.preview != null) {
-		  var fileData = {
-			file: dinner.preview,
-			path: 'dishs',
-			type: 'dish',
-			name: dinner.id + '.jpg'
-		  }
-		  SendFile(fileData)
-		  .success(function() {
-			$scope.dinnersPanel.getDinnersList($routeParams.placeId);
-		  });
-		}
-		alert('Блюдо успешно изменено, изменение превью блюда происходит в фоновом режиме, не рекомндуется перезагружать страницу браузера сразу же после измения данных блюда');
-	  })
-	  .error(function(data, state) {
-		console.log('complex dinner editing error');
-		alert('Возникла ошибка при изменении данных блюда');
-	  });
-	},
+  },
+  state: {
+    noData: false
+  },
+  dinnersList: {},
+  getDinnersList: function(placeId) {
+    console.log('placeId = ' + placeId);
+    var reqUrl = 'menu/dishlist?placeId=' + placeId;
+    console.log('try to get comples dinners list');
+    ApiRequest.get(reqUrl, true)
+    .success(function(data, state) {
+    console.log('dinners list downloaded');
+    $scope.dinnersPanel.dinnersList = data;
+    if(state == 204) {
+      console.log('no data in dinners list');
+      $scope.dinnersPanel.state.noData = true;
+    }
+    else {
+      $scope.dinnersPanel.state.noData = false;
+    }
+    })
+    .error(function(data, state) {
+    console.log('an error occured while loading dinner list');
+    });
+  },
+  addDinner: function(dish) {
+    ApiRequest.post('dish/new', {
+    name: dish.name,
+    description: dish.description,
+    cost: dish.cost,
+    dishCategoryId: dish.cat
+    }, false)
+    .success(function(data, state) {
+    SendFile({
+      file: dish.file,
+      path: 'dishs',
+      type: 'dish',
+      name: data.id + '.jpg'
+    });
+    $scope.searchPanel.add(data.id);
+    console.log('dish has been added');
+    alert('Блюдо добавлено');
+    })
+    .error(function(data, state) {
+    alert('Возникла ошибка при добавлении блюда');
+    console.log('api return error while trying to add new dish');
+    });
+  },
+  deleteDinner: function(id) {
+    if(confirm('Вы действительно хотите удалить это блюдо?')) {
+    ApiRequest.post('dish/delete', {
+      id: id
+    }, false)
+    .success(function(data, state) {
+      console.log('dinner deleted');
+      $scope.dinnersPanel.getDinnersList($routeParams.placeId);
+    })
+    .error(function(data, state) {
+      console.log('complex dinner deleting error');
+      alert('Возникла ошибка при удалении блюда');
+    });
+    }
+  },
+  editDinner: function(dinner){
+    ApiRequest.post('dish/update', {
+    id: dinner.id,
+    placeId: $routeParams.placeId,
+    name: dinner.name,
+    description: dinner.description,
+    cost: dinner.cost,
+    dishCategoryId: dinner.dishCategoryId
+    }, false)
+    .success(function(data, state) {
+    console.log('dinner edited');
+    if(dinner.preview != undefined && dinner.preview != null) {
+      var fileData = {
+      file: dinner.preview,
+      path: 'dishs',
+      type: 'dish',
+      name: dinner.id + '.jpg'
+      }
+      SendFile(fileData)
+      .success(function() {
+      $scope.dinnersPanel.getDinnersList($routeParams.placeId);
+      });
+    }
+    alert('Блюдо успешно изменено, изменение превью блюда происходит в фоновом режиме, не рекомндуется перезагружать страницу браузера сразу же после измения данных блюда');
+    })
+    .error(function(data, state) {
+    console.log('complex dinner editing error');
+    alert('Возникла ошибка при изменении данных блюда');
+    });
+  },
   }
   $scope.dinnersPanel.getDinnersList($routeParams.placeId);
 
   $scope.searchPanel = {
-	dinnersList: {},
-	search: function() {
-	  ApiRequest.get('dish/search?name=' + $scope.search.query, false)
-	  .success(function(data, state) {
-		$scope.searchPanel.dinnersList = data;
-	  });
-	},
-	add: function(id) {
-	  ApiRequest.post('menu/add', {
-		'placeId': $routeParams.placeId,
-		'dishId': id
-	  }, false)
-	  .success(function(data, state) {
-		alert('Блюдо добавлено в текущую категорию');
-		$scope.dinnersPanel.getDinnersList($routeParams.placeId);
-	  })
-	  .error(function(data, state) {
-		alert('Возникла ошибка при добавлении блюда в текущую катоегорию, попробуйте позже');
-	  });
-	}
+  dinnersList: {},
+  search: function() {
+    ApiRequest.get('dish/search?name=' + $scope.search.query, false)
+    .success(function(data, state) {
+    $scope.searchPanel.dinnersList = data;
+    });
+  },
+  add: function(id) {
+    ApiRequest.post('menu/add', {
+    'placeId': $routeParams.placeId,
+    'dishId': id
+    }, false)
+    .success(function(data, state) {
+    alert('Блюдо добавлено в текущую категорию');
+    $scope.dinnersPanel.getDinnersList($routeParams.placeId);
+    })
+    .error(function(data, state) {
+    alert('Возникла ошибка при добавлении блюда в текущую катоегорию, попробуйте позже');
+    });
+  }
   }
 }]);
--- app.js
+++ app.js
@@ -39,132 +39,157 @@ angular.module('App', [
 ]).
 config(['$routeProvider', '$locationProvider', '$sceDelegateProvider',
   function($routeProvider, $locationProvider, $sceDelegateProvider) {
-	$routeProvider.otherwise({redirectTo: '/list/1'});
-	$locationProvider.html5Mode(true);
-	$sceDelegateProvider.resourceUrlWhitelist([
-	  'self',
-	  'https://www.google.com/maps/**'
-	]);
+  $routeProvider.otherwise({redirectTo: '/list/1'});
+  $locationProvider.html5Mode(true);
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'https://www.google.com/maps/**'
+  ]);
 }]).
 run(function($location, BrowserData, AuthProvider, ApiRequest, $rootScope) {
   //AuthProvider.logIn('clain@sample.com', '199626');
 
   if(BrowserData.browser.family == 'IE' || BrowserData.browser.family == 'Opera Mini') {
-	$location.path('/badbrowser/');
+  $location.path('/badbrowser/');
   }
-	var checkUnconfirmedOrders = function() {
-		ApiRequest.get('orders/notconfirmed', false)
-		.success(function(data, state) {
-			if(data.haveOrder) {
-				console.log('user have unconfirmed orders');
-				$rootScope.notifyBtnVisible = true;
-			} else {
-				console.log('user doesnt have unconfirmed orders');
-				$rootScope.notifyBtnVisible = false;
-			}
-		});
-	}
-	checkUnconfirmedOrders();
-
-	var checkUserLogged = function() {
-		ApiRequest.get('users/current', false)
-		.success(function(data, state) {
-			if(state != 204) {
-				$rootScope.profileVisible = true;
-			} else {
-				$rootScope.profileVisible = false;
-			}
-		});
-	}
-	checkUserLogged();
-
-	$rootScope.confirmOrder = function(code) {
-		ApiRequest.post('orders/confirm', {
-			'code': code
-		}, false)
-		.success(function(data, state) {
-			alert('Заказ подтвержден');
-			$rootScope.notifyPanelExpanded = false;
-			$rootScope.notifyBtnVisible = false;
-		})
-		.error(function(data, state) {
-			alert('Введен неверный код либо заказа нет!');
-		});
-	}
-
-	$rootScope.loginForm = {
-	'data': {
-	  'loginForm': {
-		'email': null,
-		'password': null
-	  },
-	  'registrationForm': {
-		'email': null,
-		'password': null,
-		'phone': null
-	  },
-	  'forgotPasswordForm': {
-		email: null,
-		code: null,
-		newPass: null
-	  }
-	},
-	'state': {
-	  'wrongLoginEmail': false,
-	  'wrongLoginPassword': false,
-	  'failedToLogin': false,
-	  'successLogin': false,
-	  'wrondRegister': false,
-	  'successRegister': false
-	},
-	'login': function() {
-	  if($rootScope.loginForm.checkLoginForm()) {
-		console.log('try to log in on email: ' + this.data.loginForm.email + ' , password: ' + this.data.loginForm.password);
-		AuthProvider.logIn($rootScope.loginForm.data.loginForm.email, $rootScope.loginForm.data.loginForm.password).success(function(data, state) {
-		  $rootScope.signInOutModalVisible = false;
-		}).error(function(data, state) {
-		  $rootScope.loginForm.state.failedToLogin = true;
-		});
-	  } else {
-		$rootScope.loginForm.state.wrongLogin = true;
-	  }
-	},
-	'register': function() {
-	  if($rootScope.loginForm.checkRegistrationForm()) {
-		AuthProvider.register($rootScope.loginForm.data.registrationForm.phone, $rootScope.loginForm.data.registrationForm.email, $rootScope.loginForm.data.registrationForm.password).success(function() {
-		  $rootScope.loginForm.state.successRegister = true;
-		  $rootScope.loginForm.state.wrongRegister = false;
-		  console.log('registration success');
-		}).error(function(){
-		  $rootScope.loginForm.state.wrongRegister = true;
-		});
-	  } else {
-		$rootScope.loginForm.state.wrongRegister = true;
-	  }
-	},
-	'resetPassword': function() {
-
-	},
-	'checkLoginForm': function() {
-	  if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.loginForm.password)) {
-		if((/^(.)*@(.)*\.(.){2,}$/).test($rootScope.loginForm.data.loginForm.email)) {
-		  return true;
-		} else {
-		  $rootScope.loginForm.state.wrongLoginEmail = true;
-		  return false;
-		}
-	  } else {
-		$rootScope.loginForm.state.wrongLoginPassword = true;
-		return false;
-	  }
-	},
-	'checkRegistrationForm': function() {
-	  if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.registrationForm.password) && (/^(.){1,}@(.){2,}\.(.){2,10}$/).test($rootScope.loginForm.data.registrationForm.email) && (/^(\d){12}$/).test($rootScope.loginForm.data.registrationForm.phone)) {
-		return true;
-	  } else {
-		return false;
-	  }
-	}
+  var checkUnconfirmedOrders = function() {
+    ApiRequest.get('orders/notconfirmed', false)
+    .success(function(data, state) {
+      if(data.haveOrder) {
+        console.log('user have unconfirmed orders');
+        $rootScope.notifyBtnVisible = true;
+      } else {
+        console.log('user doesnt have unconfirmed orders');
+        $rootScope.notifyBtnVisible = false;
+      }
+    });
   }
+  checkUnconfirmedOrders();
+
+  var checkUserLogged = function() {
+    ApiRequest.get('users/current', false)
+    .success(function(data, state) {
+      if(state != 204) {
+        $rootScope.profileVisible = true;
+      } else {
+        $rootScope.profileVisible = false;
+      }
+    });
+  }
+  checkUserLogged();
+
+  $rootScope.confirmOrder = function(code) {
+    ApiRequest.post('orders/confirm', {
+      'code': code
+    }, false)
+    .success(function(data, state) {
+      alert('Заказ подтвержден');
+      $rootScope.notifyPanelExpanded = false;
+      $rootScope.notifyBtnVisible = false;
+    })
+    .error(function(data, state) {
+      alert('Введен неверный код либо заказа нет!');
+    });
+  }
+
+  $rootScope.loginForm = {
+  'data': {
+    'loginForm': {
+    'email': null,
+    'password': null
+    },
+    'registrationForm': {
+    'email': null,
+    'password': null,
+    'phone': null
+    },
+    'forgotPasswordForm': {
+    email: null,
+    code: null,
+    newPass: null
+    }
+  },
+  'state': {
+    'wrongLoginEmail': false,
+    'wrongLoginPassword': false,
+    'failedToLogin': false,
+    'successLogin': false,
+    'wrondRegister': false,
+    'successRegister': false
+  },
+  'login': function() {
+    if($rootScope.loginForm.checkLoginForm()) {
+    console.log('try to log in on email: ' + this.data.loginForm.email + ' , password: ' + this.data.loginForm.password);
+    AuthProvider.logIn($rootScope.loginForm.data.loginForm.email, $rootScope.loginForm.data.loginForm.password).success(function(data, state) {
+      checkUserLogged();
+      checkUnconfirmedOrders();
+      $rootScope.signInOutModalVisible = false;
+    }).error(function(data, state) {
+      $rootScope.loginForm.state.failedToLogin = true;
+    });
+    } else {
+    $rootScope.loginForm.state.wrongLogin = true;
+    }
+  },
+  'register': function() {
+    if($rootScope.loginForm.checkRegistrationForm()) {
+    AuthProvider.register($rootScope.loginForm.data.registrationForm.phone, $rootScope.loginForm.data.registrationForm.email, $rootScope.loginForm.data.registrationForm.password).success(function() {
+      $rootScope.loginForm.state.successRegister = true;
+      $rootScope.loginForm.state.wrongRegister = false;
+      console.log('registration success');
+    }).error(function(){
+      $rootScope.loginForm.state.wrongRegister = true;
+    });
+    } else {
+    $rootScope.loginForm.state.wrongRegister = true;
+    }
+  },
+  'resetPassword': function() {
+
+  },
+  'checkLoginForm': function() {
+    if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.loginForm.password)) {
+    if((/^(.)*@(.)*\.(.){2,}$/).test($rootScope.loginForm.data.loginForm.email)) {
+      return true;
+    } else {
+      $rootScope.loginForm.state.wrongLoginEmail = true;
+      return false;
+    }
+    } else {
+    $rootScope.loginForm.state.wrongLoginPassword = true;
+    return false;
+    }
+  },
+  'checkRegistrationForm': function() {
+    if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.registrationForm.password) && (/^(.){1,}@(.){2,}\.(.){2,10}$/).test($rootScope.loginForm.data.registrationForm.email) && (/^(\d){12}$/).test($rootScope.loginForm.data.registrationForm.phone)) {
+    return true;
+    } else {
+    return false;
+    }
+  },
+    restorePassword: function(phone) {
+      ApiRequest.post('users/restore', {
+        'phone': phone
+      }, false)
+      .success(function(data, state) {
+        $rootScope.regLogModalTabSelected = 3;
+      })
+      .error(function(data, state) {
+      });
+    },
+    changePassword(code, password) {
+      ApiRequest.post('users/confirm', {
+        code: code,
+        newPass: password
+      }, false)
+      .success(function(data, state) {
+        alert('Пароль изменен');
+        $scope.wrongCode = false;
+      })
+      .error(function(data, state) {
+        $rootScope.wrongCode = true;
+      });
+    }
+}
 
 });
