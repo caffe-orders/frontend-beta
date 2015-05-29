c20842b0cc6a5a61474e419b25100841ea854fa9
ClainAtomi <clainatomi.ctb@gmail.com>
2015-05-29 17:59:51 +0300

    notify btn issue fix

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ config(['$routeProvider', '$locationProvider', '$sceDelegateProvider',
   $routeProvider.otherwise({redirectTo: '/list/1'});
   $locationProvider.html5Mode(true);
   $sceDelegateProvider.resourceUrlWhitelist([
-    'self',
-    'https://www.google.com/maps/**'
+	'self',
+	'https://www.google.com/maps/**'
   ]);
 }]).
 run(function($location, BrowserData, AuthProvider, ApiRequest, $rootScope) {
@@ -53,145 +53,145 @@ run(function($location, BrowserData, AuthProvider, ApiRequest, $rootScope) {
   $location.path('/badbrowser/');
   }
   var checkUnconfirmedOrders = function() {
-    ApiRequest.get('orders/notconfirmed', false)
-    .success(function(data, state) {
-      if(data.haveOrder) {
-        console.log('user have unconfirmed orders');
-        $rootScope.notifyBtnVisible = true;
-      } else {
-        console.log('user doesnt have unconfirmed orders');
-        $rootScope.notifyBtnVisible = false;
-      }
-    });
+	ApiRequest.get('orders/notconfirmed', false)
+	.success(function(data, state) {
+	  if(data[0]) {
+		console.log('user have unconfirmed orders');
+		$rootScope.notifyBtnVisible = true;
+	  } else {
+		console.log('user doesnt have unconfirmed orders');
+		$rootScope.notifyBtnVisible = false;
+	  }
+	});
   }
   checkUnconfirmedOrders();
 
   var checkUserLogged = function() {
-    ApiRequest.get('users/current', false)
-    .success(function(data, state) {
-      if(state != 204) {
-        $rootScope.profileVisible = true;
-      } else {
-        $rootScope.profileVisible = false;
-      }
-    });
+	ApiRequest.get('users/current', false)
+	.success(function(data, state) {
+	  if(state != 204) {
+		$rootScope.profileVisible = true;
+	  } else {
+		$rootScope.profileVisible = false;
+	  }
+	});
   }
   checkUserLogged();
 
   $rootScope.confirmOrder = function(code) {
-    ApiRequest.post('orders/confirm', {
-      'code': code
-    }, false)
-    .success(function(data, state) {
-      alert('Заказ подтвержден');
-      $rootScope.notifyPanelExpanded = false;
-      $rootScope.notifyBtnVisible = false;
-    })
-    .error(function(data, state) {
-      alert('Введен неверный код либо заказа нет!');
-    });
+	ApiRequest.post('orders/confirm', {
+	  'code': code
+	}, false)
+	.success(function(data, state) {
+	  alert('Заказ подтвержден');
+	  $rootScope.notifyPanelExpanded = false;
+	  $rootScope.notifyBtnVisible = false;
+	})
+	.error(function(data, state) {
+	  alert('Введен неверный код либо заказа нет!');
+	});
   }
 
   $rootScope.loginForm = {
   'data': {
-    'loginForm': {
-    'email': null,
-    'password': null
-    },
-    'registrationForm': {
-    'email': null,
-    'password': null,
-    'phone': null
-    },
-    'forgotPasswordForm': {
-    email: null,
-    code: null,
-    newPass: null
-    }
+	'loginForm': {
+	'email': null,
+	'password': null
+	},
+	'registrationForm': {
+	'email': null,
+	'password': null,
+	'phone': null
+	},
+	'forgotPasswordForm': {
+	email: null,
+	code: null,
+	newPass: null
+	}
   },
   'state': {
-    'wrongLoginEmail': false,
-    'wrongLoginPassword': false,
-    'failedToLogin': false,
-    'successLogin': false,
-    'wrondRegister': false,
-    'successRegister': false
+	'wrongLoginEmail': false,
+	'wrongLoginPassword': false,
+	'failedToLogin': false,
+	'successLogin': false,
+	'wrondRegister': false,
+	'successRegister': false
   },
   'login': function() {
-    if($rootScope.loginForm.checkLoginForm()) {
-    console.log('try to log in on email: ' + this.data.loginForm.email + ' , password: ' + this.data.loginForm.password);
-    AuthProvider.logIn($rootScope.loginForm.data.loginForm.email, $rootScope.loginForm.data.loginForm.password).success(function(data, state) {
-      checkUserLogged();
-      checkUnconfirmedOrders();
-      $rootScope.signInOutModalVisible = false;
-    }).error(function(data, state) {
-      $rootScope.loginForm.state.failedToLogin = true;
-    });
-    } else {
-    $rootScope.loginForm.state.wrongLogin = true;
-    }
+	if($rootScope.loginForm.checkLoginForm()) {
+	console.log('try to log in on email: ' + this.data.loginForm.email + ' , password: ' + this.data.loginForm.password);
+	AuthProvider.logIn($rootScope.loginForm.data.loginForm.email, $rootScope.loginForm.data.loginForm.password).success(function(data, state) {
+	  checkUserLogged();
+	  checkUnconfirmedOrders();
+	  $rootScope.signInOutModalVisible = false;
+	}).error(function(data, state) {
+	  $rootScope.loginForm.state.failedToLogin = true;
+	});
+	} else {
+	$rootScope.loginForm.state.wrongLogin = true;
+	}
   },
   'register': function() {
-    if($rootScope.loginForm.checkRegistrationForm()) {
-    AuthProvider.register($rootScope.loginForm.data.registrationForm.phone, $rootScope.loginForm.data.registrationForm.email, $rootScope.loginForm.data.registrationForm.password).success(function() {
-      $rootScope.loginForm.state.successRegister = true;
-      $rootScope.loginForm.state.wrongRegister = false;
-      console.log('registration success');
-    }).error(function(){
-      $rootScope.loginForm.state.wrongRegister = true;
-    });
-    } else {
-    $rootScope.loginForm.state.wrongRegister = true;
-    }
+	if($rootScope.loginForm.checkRegistrationForm()) {
+	AuthProvider.register($rootScope.loginForm.data.registrationForm.phone, $rootScope.loginForm.data.registrationForm.email, $rootScope.loginForm.data.registrationForm.password).success(function() {
+	  $rootScope.loginForm.state.successRegister = true;
+	  $rootScope.loginForm.state.wrongRegister = false;
+	  console.log('registration success');
+	}).error(function(){
+	  $rootScope.loginForm.state.wrongRegister = true;
+	});
+	} else {
+	$rootScope.loginForm.state.wrongRegister = true;
+	}
   },
   'resetPassword': function() {
 
   },
   'checkLoginForm': function() {
-    if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.loginForm.password)) {
-    if((/^(.)*@(.)*\.(.){2,}$/).test($rootScope.loginForm.data.loginForm.email)) {
-      return true;
-    } else {
-      $rootScope.loginForm.state.wrongLoginEmail = true;
-      return false;
-    }
-    } else {
-    $rootScope.loginForm.state.wrongLoginPassword = true;
-    return false;
-    }
+	if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.loginForm.password)) {
+	if((/^(.)*@(.)*\.(.){2,}$/).test($rootScope.loginForm.data.loginForm.email)) {
+	  return true;
+	} else {
+	  $rootScope.loginForm.state.wrongLoginEmail = true;
+	  return false;
+	}
+	} else {
+	$rootScope.loginForm.state.wrongLoginPassword = true;
+	return false;
+	}
   },
   'checkRegistrationForm': function() {
-    if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.registrationForm.password) && (/^(.){1,}@(.){2,}\.(.){2,10}$/).test($rootScope.loginForm.data.registrationForm.email) && (/^(\d){12}$/).test($rootScope.loginForm.data.registrationForm.phone)) {
-    return true;
-    } else {
-    return false;
-    }
+	if((/^((\d)|(\w)){5,18}$/).test($rootScope.loginForm.data.registrationForm.password) && (/^(.){1,}@(.){2,}\.(.){2,10}$/).test($rootScope.loginForm.data.registrationForm.email) && (/^(\d){12}$/).test($rootScope.loginForm.data.registrationForm.phone)) {
+	return true;
+	} else {
+	return false;
+	}
   },
-    restorePassword: function(phone) {
-      ApiRequest.post('users/restore', {
-        'phone': phone
-      }, false)
-      .success(function(data, state) {
-        $rootScope.regLogModalTabSelected = 3;
-      })
-      .error(function(data, state) {
-        alert('Возможно у вас есть неподтвержденный запрос на восстановление');
-        $rootScope.regLogModalTabSelected = 3;
-      });
-    },
-    changePassword(code, password) {
-      ApiRequest.post('users/confirm', {
-        code: code,
-        newPass: password
-      }, false)
-      .success(function(data, state) {
-        alert('Пароль изменен');
-        $scope.wrongCode = false;
-      })
-      .error(function(data, state) {
-        $rootScope.wrongCode = true;
-      });
-    }
+	restorePassword: function(phone) {
+	  ApiRequest.post('users/restore', {
+		'phone': phone
+	  }, false)
+	  .success(function(data, state) {
+		$rootScope.regLogModalTabSelected = 3;
+	  })
+	  .error(function(data, state) {
+		alert('Возможно у вас есть неподтвержденный запрос на восстановление');
+		$rootScope.regLogModalTabSelected = 3;
+	  });
+	},
+	changePassword(code, password) {
+	  ApiRequest.post('users/confirm', {
+		code: code,
+		newPass: password
+	  }, false)
+	  .success(function(data, state) {
+		alert('Пароль изменен');
+		$scope.wrongCode = false;
+	  })
+	  .error(function(data, state) {
+		$rootScope.wrongCode = true;
+	  });
+	}
 }
 
 });
